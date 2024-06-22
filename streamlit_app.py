70f8e1057639d9c3ae05d709d8786354c604b798
Prasad527 <31573304+Prasad527@users.noreply.github.com>
2024-06-22 11:56:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,7 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+
 
 ingredients_list = st.multiselect('Choose uptop 5 ingredeints:',my_dataframe,max_selections=5)
 
