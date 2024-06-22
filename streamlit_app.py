e5404d57ac4d72b1b13f3c500c27c4e33f021e3c
Prasad527 <31573304+Prasad527@users.noreply.github.com>
2024-06-22 11:43:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,9 @@ st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
 pd_df = my_dataframe.to_pandas()
-# st.dataframe(pd_df)
-# st.stop()
+st.dataframe(pd_df)
+st.stop()
+
 ingredients_list = st.multiselect(
     'Choose uptop 5 ingredeints:',
     my_dataframe,
